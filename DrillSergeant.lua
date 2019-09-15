47bbb368b9a98e05c97c6e1faee31166b7a40474
KyrosKrane <kyros@kyros.info>
2019-09-15 02:37:55 -0500

    Initial implementation of mouseover identification of what rare will spawn from a Broken Drill Rig. You'll see an extra line in your tooltip. If you're too far from the broken rig, YOU COULD GET THE WRONG RARE. So move very close to the rig before mousing over it.

--- DrillSergeant.lua
+++ DrillSergeant.lua
@@ -30,6 +30,14 @@ local pairs = pairs
 local Driller = {}
 
 
+
+--#########################################
+--# Libraries
+--#########################################
+
+local HBD = LibStub("HereBeDragons-2.0")
+
+
 --#########################################
 --# Frame for event handling
 --#########################################
@@ -53,27 +61,29 @@ local CHAT_GREEN = "|cff" .. "00ff00"
 local CHAT_BLUE = "|cff" .. "0066ff"
 local CHAT_RED = "|cff" .. "a00000"
 
+local MECHAGON_MAPID = 123 -- #TODO: Update this
+
 
 -- The mobs and locs identified by each drill rig
 Driller.Projects = {
-	["DR-CC61"] = {Mob = "Gorged Gear-Cruncher", Loc = "73.0, 54.2"}, -- loc confirmed
-	["DR-CC73"] = {Mob = "Caustic Mechaslime", Loc = "66.5, 58.8"}, -- loc confirmed
-	["DR-CC88"] = {Mob = "The Kleptoboss", Loc = "68.4, 48.1"},
+	["DR-CC61"] = {Mob = "Gorged Gear-Cruncher", Loc = {x = 73.0, y = 54.2}}, -- loc confirmed
+	["DR-CC73"] = {Mob = "Caustic Mechaslime", Loc = {x = 66.5, y = 58.8}}, -- loc confirmed
+	["DR-CC88"] = {Mob = "The Kleptoboss", Loc = {x = 68.4, y = 48.1}},
 
-	["DR-JD41"] = {Mob = "Boilburn", Loc = "51.1, 50.3"}, -- loc confirmed
-	["DR-JD99"] = {Mob = "Gemicide", Loc = "59.7, 67.2"},
+	["DR-JD41"] = {Mob = "Boilburn", Loc = {x = 51.1, y = 50.3}}, -- loc confirmed
+	["DR-JD99"] = {Mob = "Gemicide", Loc = {x = 59.7, y = 67.2}},
 
-	["DR-TR28"] = {Mob = "Ol' Big Tusk", Loc = "56.2, 36.3"},
-	["DR-TR35"] = {Mob = "Earthbreaker Gulroc", Loc = "63.2, 25.4"}, -- loc confirmed
+	["DR-TR28"] = {Mob = "Ol' Big Tusk", Loc = {x = 56.2, y = 36.3}},
+	["DR-TR35"] = {Mob = "Earthbreaker Gulroc", Loc = {x = 63.2, y = 25.4}}, -- loc confirmed
 
 	--@alpha@
 	-- testing only
-	["DR-Fake872"] = {Mob = "Automated flame turret (?? from 872)", Loc = "123, 456"},
-	["DR-Fake951"] = {Mob = "Automated flame turret (?? from 951)", Loc = "123, 456"},
-	["DR-Fake952"] = {Mob = "Automated flame turret (149879 from 952)", Loc = "123, 456"},
-	["DR-Fake955"] = {Mob = "Automated flame turret (149879 from 955)", Loc = "123, 456"},
-	["DR-Fake456"] = {Mob = "Auria Irondreamer", Loc = "123, 456"},
-	--["DR-Fake789"] = {Mob = "Izzy Hollyfizzle", Loc = "123, 456"},
+	["DR-Fake872"] = {Mob = "Automated flame turret (?? from 872)", Loc = {x = 1, y = 2}},
+	["DR-Fake951"] = {Mob = "Automated flame turret (?? from 951)", Loc = {x = 1, y = 2}},
+	["DR-Fake952"] = {Mob = "Automated flame turret (149879 from 952)", Loc = {x = 1, y = 2}},
+	["DR-Fake955"] = {Mob = "Automated flame turret (149879 from 955)", Loc = {x = 1, y = 2}},
+	["DR-Fake456"] = {Mob = "Auria Irondreamer", Loc = {x = 1, y = 2}},
+	--["DR-Fake789"] = {Mob = "Izzy Hollyfizzle", Loc = {x = 1, y = 2}},
 	--@end-alpha@
 } -- Driller.Projects
 
@@ -149,6 +159,38 @@ function Driller:DumpTable(tab, indent)
 		end
 	end
 end -- Driller:DumpTable()
+
+
+-- This function determines whether a point S is inside a triangle described by points A, B, and C.
+-- Returns true (inside) or false (outside)
+-- Adapted from the answer by John Bananas here: https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+-- s, a, b, and c must all be objects (tables) with two elements named x and y.
+function IsInsideTriangle(s, a, b, c)
+    local as_x = s.x-a.x
+    local as_y = s.y-a.y
+
+    local s_ab = (b.x-a.x)*as_y-(b.y-a.y)*as_x > 0
+
+	if (c.x-a.x)*as_y-(c.y-a.y)*as_x > 0 == s_ab then return false end
+
+	if (c.x-b.x)*(s.y-b.y)-(c.y-b.y)*(s.x-b.x) > 0 ~= s_ab then return false end
+
+    return true
+end -- IsInsideTriangle()
+
+
+--[===[
+-- Test case
+Gorged  = {x = 73.0, y = 54.2}
+Caustic = {x = 66.5, y = 58.8}
+Klepto  = {x = 68.4, y = 48.1}
+
+outside = {x = 1, y = 1}
+inside = {x = 68, y = 54}
+
+if IsInsideTriangle(inside, Gorged, Caustic, Klepto) then print "inside reports true - PASS" else print "inside reports false - FAIL" end
+if IsInsideTriangle(outside, Gorged, Caustic, Klepto) then print "outside reports true - FAIL" else print "outside reports false - PASS" end
+-- ]===]
 
 
 --#########################################
@@ -228,14 +270,107 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 	end
 
 	--Driller:DebugPrint("found ID " .. NPCID)
-
-	local ProjectID = Driller.MobIDs[NPCID]
-	if not ProjectID then
-		--Driller:DebugPrint("no Project ID found in MobIDs for NPCID ".. NPCID)
+	if not (154695 == NPCID or 154933 == NPCID or 150277 == NPCID) then
+		-- not a tracked ID
+		--Driller:DebugPrint("Not a tracked NPC.")
 		return
 	end
 
+	-- Get player coordinates
+	local PlayerX, PlayerY, PinstanceID = HBD:GetPlayerWorldPosition()
+	Driller:DebugPrint("PlayerX is " .. PlayerX)
+	Driller:DebugPrint("PlayerY is " .. PlayerY)
+	Driller:DebugPrint("PinstanceID is " .. PinstanceID)
+
+	local ProjectID, MobX, MobY-- used for finding the range
+
+	if 154695 == NPCID then
+		-- could be:
+		-- "DR-CC61", -- "Gorged Gear-Cruncher"
+		-- "DR-CC73", -- "Caustic Mechaslime"
+		-- "DR-CC88", -- "The Kleptoboss"
+		Driller:DebugPrint("In CC block.")
+
+
+
+		-- Find out which mob is closest
+		MobX, MobY = HBD:GetWorldCoordinatesFromZone(Driller.Projects["DR-CC61"].Loc.x, Driller.Projects["DR-CC61"].Loc.y, PinstanceID)
+		local RangeToGearCruncher = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, MobX, MobY)
+
+		MobX, MobY = HBD:GetWorldCoordinatesFromZone(Driller.Projects["DR-CC73"].Loc.x, Driller.Projects["DR-CC73"].Loc.y, PinstanceID)
+		local RangeToMechaslime = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, MobX, MobY)
+
+		MobX, MobY = HBD:GetWorldCoordinatesFromZone(Driller.Projects["DR-CC88"].Loc.x, Driller.Projects["DR-CC88"].Loc.y, PinstanceID)
+		local RangeToKleptoboss = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, MobX, MobY)
+
+		Driller:DebugPrint("RangeToGearCruncher is " .. RangeToGearCruncher)
+		Driller:DebugPrint("RangeToMechaslime is " .. RangeToMechaslime)
+		Driller:DebugPrint("RangeToKleptoboss is " .. RangeToKleptoboss)
+		if RangeToGearCruncher < RangeToMechaslime and RangeToGearCruncher < RangeToKleptoboss then
+			Driller:DebugPrint("Picking DR-CC61 Gorged Gear-Cruncher")
+			ProjectID = "DR-CC61" -- "Gorged Gear-Cruncher"
+		elseif RangeToMechaslime < RangeToGearCruncher and RangeToMechaslime < RangeToKleptoboss then
+			Driller:DebugPrint("Picking DR-CC73 Caustic Mechaslime")
+			ProjectID = "DR-CC73" -- "Caustic Mechaslime"
+		else
+			Driller:DebugPrint("Picking DR-CC88 Kleptoboss")
+			ProjectID = "DR-CC88" -- "Kleptoboss"
+		end
+
+	elseif 154933 == NPCID then
+		-- could be:
+		-- "DR-JD41", -- "Boilburn"
+		-- "DR-JD99", -- "Gemicide"
+		Driller:DebugPrint("In JD block.")
+
+		-- Find out which is closer
+		MobX, MobY = HBD:GetWorldCoordinatesFromZone(Driller.Projects["DR-JD41"].Loc.x, Driller.Projects["DR-JD41"].Loc.y, PinstanceID)
+		Driller:DebugPrint("MobX is " .. MobX)
+		Driller:DebugPrint("MobY is " .. MobY)
+
+		local RangeToBoilburn = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, MobX, MobY)
+
+		MobX, MobY = HBD:GetWorldCoordinatesFromZone(Driller.Projects["DR-JD99"].Loc.x, Driller.Projects["DR-JD99"].Loc.y, PinstanceID)
+		local RangeToGemicide = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, MobX, MobY)
+
+		Driller:DebugPrint("RangeToBoilburn is " .. RangeToBoilburn)
+		Driller:DebugPrint("RangeToGemicide is " .. RangeToGemicide)
+		if RangeToBoilburn < RangeToGemicide then
+			Driller:DebugPrint("Picking DR-JD41 Boilburn")
+			ProjectID = "DR-JD41" -- "Boilburn"
+		else
+			Driller:DebugPrint("Picking DR-JD99 Gemicide")
+			ProjectID = "DR-JD99" -- "Gemicide"
+		end
+	elseif 150277 == NPCID then
+		-- could be:
+		-- "DR-TR28", -- "Ol' Big Tusk"
+		-- "DR-TR35", -- "Earthbreaker Gulroc"
+		Driller:DebugPrint("In TR block.")
+
+		-- Find out which is closer, BigTusk or Gulroc
+		local RangeToBigTusk = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, Driller.Projects["DR-TR28"].Loc.x, Driller.Projects["DR-TR28"].Loc.y)
+		local RangeToGulroc = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, Driller.Projects["DR-TR35"].Loc.x, Driller.Projects["DR-TR35"].Loc.y)
+		Driller:DebugPrint("RangeToBigTusk is " .. RangeToBigTusk)
+		Driller:DebugPrint("RangeToGulroc is " .. RangeToGulroc)
+		if RangeToBigTusk < RangeToGulroc then
+			Driller:DebugPrint("Picking DR-TR28 Ol' Big Tusk")
+			ProjectID = "DR-TR28" -- "Ol' Big Tusk"
+		else
+			Driller:DebugPrint("Picking DR-TR35 Earthbreaker Gulroc")
+			ProjectID = "DR-TR35" -- "Earthbreaker Gulroc"
+		end
+	else
+		-- This should be categorically impossible, since we already validated the ID a moment ago.
+		Driller:DebugPrint("Reached the impossible block. Fix your code, Kyros.")
+		return
+	end
+
+	if not ProjectID then	return	end
+
 	Driller:DebugPrint("NPCID is " .. NPCID ..", ProjectID is " .. ProjectID)
+
+
 
 	local Project = Driller.Projects[ProjectID]
 	if not Project then
@@ -244,6 +379,7 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 	end
 
 	local Mob = Project.Mob
+	-- local Loc = Project.Loc
 
 	Driller:DebugPrint("match found in MobIDs: " .. Mob)
 	GameTooltip:AddLine(ProjectID .. " opens a path to " .. CHAT_GREEN .. Mob .. FONT_COLOR_CODE_CLOSE)
@@ -269,10 +405,11 @@ function Driller.Events:CHAT_MSG_MONSTER_EMOTE(...)
 		Driller:DebugPrint("Identified DrillID " .. DrillID)
 		if Driller.Projects[DrillID] then
 			Driller:DebugPrint("mob is >>" .. Driller.Projects[DrillID].Mob .. "<<")
-			Driller:DebugPrint("loc is >>" .. Driller.Projects[DrillID].Loc .. "<<")
+			local Loc = Driller.Projects[DrillID].Loc.x .. ", " .. Driller.Projects[DrillID].Loc.y
+			Driller:DebugPrint("loc is >>" .. Loc .. "<<")
 
 			-- Found a proper drill message. Notify the user.
-			Driller:ChatPrint(CHAT_GREEN .. Driller.Projects[DrillID].Mob .. FONT_COLOR_CODE_CLOSE .. " is about to spawn at location " .. Driller.Projects[DrillID].Loc .. " in one minute.")
+			Driller:ChatPrint(CHAT_GREEN .. Driller.Projects[DrillID].Mob .. FONT_COLOR_CODE_CLOSE .. " is about to spawn at location " .. Loc .. " in one minute.")
 		else
 			Driller:ChatPrint("Unknown Drill ID " .. DrillID .. ". Please report this message and the Drill Rig message right above (or below) it to the addon author for investigation.")
 		end

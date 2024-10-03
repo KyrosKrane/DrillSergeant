3483af277dab4888dc66cde339b1bc7dc65a2133
KyrosKrane Sylvanblade <kyros@kyros.info>
2024-10-03 13:59:52 -0500

    Enable TomTom integration (#4)
    Drill Sergeant will now use TomTom's arrows by default if it's installed. You can change this by using one of these commands:
    
    /drill wow 
    /drill tomtom
    
    Closes #3.

--- DrillSergeant.lua
+++ DrillSergeant.lua
@@ -1,6 +1,6 @@
 -- DrillSergeant.lua
 -- Written by KyrosKrane Sylvanblade (kyros@kyros.info)
--- Copyright (c) 2019-2022 KyrosKrane Sylvanblade
+-- Copyright (c) 2019-2024 KyrosKrane Sylvanblade
 -- Licensed under the MIT License, as per the included file.
 
 -- File revision: @file-abbreviated-hash@
@@ -35,6 +35,7 @@ local addonName, Driller = ...
 --@alpha@
 Driller.DebugMode = true
 --@end-alpha@
+local DebugPrint = Driller.Utilities.DebugPrint
 
 
 --#########################################
@@ -125,6 +126,73 @@ end
 
 
 --#########################################
+--# Waypoint link creation and click handling
+--#########################################
+
+-- Creates a clickable chat link based on the selected game mode.
+-- map is the MapID
+-- X and Y should be 0.00-100.00
+-- Name is the name of the thing the waypoint is pointing to (only relevant for TomTom)
+local function CreateWaypointLink(map, x, y, name)
+	DebugPrint("In CreateWaypointLink")
+
+	local DisplayLoc = x .. ", " .. y
+	DebugPrint("DisplayLoc is >>" .. DisplayLoc .. "<<")
+	local LocWithLink
+
+	if "TomTom" == Driller.mode then
+		DebugPrint("TomTom mode detected")
+
+		LocWithLink = string.format("|cffffff00|Haddon:" .. addonName .. ":%s:%s:%s:%s|h[|A:bags-greenarrow:22:22|a%s]|h|r", map, x, y, name, DisplayLoc)
+		DebugPrint("LocWithLink is >>" .. LocWithLink .. "<<")
+
+	else
+		DebugPrint("WoW mode detected")
+
+		-- WoW links expect x and y to be 0-10000.
+		LocWithLink = string.format("|cffffff00|Hworldmap:%s:%s:%s|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a %s]|h|r", map, x * 100, y * 100, DisplayLoc)
+		DebugPrint("LocWithLink is >>" .. LocWithLink .. "<<")
+	end
+
+	return LocWithLink
+end -- CreateWaypointLink()
+
+
+-- Chat link click handling - only required for TomTom
+local function HandleClickCallback(event, link, text, button, chatFrame)
+	local linkType, linkAddonName, mapID, x, y, name = strsplit(":", link)
+	--DebugPrint("Received callback for SetItemRef, linktype is ", linkType, ", linkAddonName is ", linkAddonName)
+
+	-- Make sure it's our link, otherwise bail
+	if linkType ~= "addon" or linkAddonName ~= addonName then return end
+
+	-- HereBeDragons type-checks its parameters to be numbers, so coerce them to the right type.
+	-- Both TomTom and WoW expect x and y to be 0-1, not 0-100
+	mapID = 0 + mapID
+	x = x / 100
+	y = y / 100
+
+	DebugPrint(format("DS link detected. mapID = %s, x = %s, y = %s, name = %s", mapID, x, y, name))
+
+	if Driller.mode == "TomTom" then
+		DebugPrint("Dispatching TomTom")
+		TomTom:SetCustomWaypoint(mapID, x, y, { title = name, from = Driller.USER_ADDON_NAME })
+	else
+		-- This is an odd situation. The user has TomTom loaded and configued in DS, and a mob spawned, causing a link to be displayed. The user then swapped their config to WoW waypoints and clicked the link.
+		-- So, we manually show the WoW link.
+		DebugPrint("Dispatching WoW")
+		local mappoint = UiMapPoint.CreateFromCoordinates(mapID, x, y)
+		C_Map.SetUserWaypoint(mappoint)
+		C_SuperTrack.SetSuperTrackedUserWaypoint(true)
+	end
+
+end -- HandleClickCallback()
+
+EventRegistry:RegisterCallback("SetItemRef", HandleClickCallback)
+
+
+
+--#########################################
 --# Tooltip detection and management
 --#########################################
 
@@ -140,8 +208,8 @@ end
 -- Dragonflight update: OnTooltipSetUnit is removed from the GameTooltip, and instead has to be written locally then linked.
 -- Self in the parameter list corresponds to the tooltip (so the proper parameters would be (tooltip, data) ).
 local function OnTooltipSetUnit(self, data)
-    -- Only process GameTooltip
-    if not self == GameTooltip then return end
+	-- Only process GameTooltip
+	if not self == GameTooltip then return end
 
 	-- Don't process if we're in a pet battle
 	if C_PetBattles.IsInBattle() then return end
@@ -155,7 +223,7 @@ local function OnTooltipSetUnit(self, data)
 	-- if HBD returns invalid X or Y values (usually because the client is too busy), bail out so we don't throw user errors.
 	if not PlayerX or not PlayerY then return end
 
-	--Driller.Utilities.DebugPrint("PlayerX is " .. (PlayerX or "nil") .. ", PlayerY is " .. (PlayerY or "nil") .. ", PinstanceID is " .. (PinstanceID or "nil"))
+	--DebugPrint("PlayerX is " .. (PlayerX or "nil") .. ", PlayerY is " .. (PlayerY or "nil") .. ", PinstanceID is " .. (PinstanceID or "nil"))
 
 	-- Find out what unit is being moused over
 	local unit = select(2, self:GetUnit())
@@ -163,7 +231,7 @@ local function OnTooltipSetUnit(self, data)
 
 	-- get details on the unit, and make sure it's not a player.
 	local guid = UnitGUID(unit) or ""
-	--Driller.Utilities.DebugPrint("guid is " .. (guid or "nil"))
+	--DebugPrint("guid is " .. (guid or "nil"))
 	-- GUID format
 	-- [Unit type]-0-[server ID]-[instance ID]-[zone UID]-[ID]-[spawn UID]
 	-- (Example: "Creature-0-970-0-11-31146-000136DF91")
@@ -172,7 +240,7 @@ local function OnTooltipSetUnit(self, data)
 	local IsPlayer = guid:match("%a+") == "Player"
 	if IsPlayer or not NPCID then return end
 
-	local ProjectID  -- used to identify the right project
+	local ProjectID -- used to identify the right project
 	local InRange = true -- is the mob in ID range?
 
 	if 154695 == NPCID then
@@ -180,28 +248,28 @@ local function OnTooltipSetUnit(self, data)
 		-- "DR-CC61", -- "Gorged Gear-Cruncher"
 		-- "DR-CC73", -- "Caustic Mechaslime"
 		-- "DR-CC88", -- "The Kleptoboss"
-		Driller.Utilities.DebugPrint("In CC block.")
+		DebugPrint("In CC block.")
 
 		-- Find out which mob is closest
 		local RangeToGearCruncher = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, Driller.Projects["DR-CC61"].WorldLoc.x, Driller.Projects["DR-CC61"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToGearCruncher is " .. Driller.Projects["DR-CC61"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC61"].WorldLoc.y .. ", " .. RangeToGearCruncher)
+		DebugPrint("MobX, MobY, RangeToGearCruncher is " .. Driller.Projects["DR-CC61"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC61"].WorldLoc.y .. ", " .. RangeToGearCruncher)
 
 		local RangeToMechaslime = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, Driller.Projects["DR-CC73"].WorldLoc.x, Driller.Projects["DR-CC73"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToMechaslime is " .. Driller.Projects["DR-CC73"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC73"].WorldLoc.y .. ", " .. RangeToMechaslime)
+		DebugPrint("MobX, MobY, RangeToMechaslime is " .. Driller.Projects["DR-CC73"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC73"].WorldLoc.y .. ", " .. RangeToMechaslime)
 
 		local RangeToKleptoboss = HBD:GetWorldDistance(PinstanceID, PlayerX, PlayerY, Driller.Projects["DR-CC88"].WorldLoc.x, Driller.Projects["DR-CC88"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToKleptoboss is " .. Driller.Projects["DR-CC88"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC88"].WorldLoc.y .. ", " .. RangeToKleptoboss)
+		DebugPrint("MobX, MobY, RangeToKleptoboss is " .. Driller.Projects["DR-CC88"].WorldLoc.x .. ", " .. Driller.Projects["DR-CC88"].WorldLoc.y .. ", " .. RangeToKleptoboss)
 
 		if RangeToGearCruncher <= RangeToMechaslime and RangeToGearCruncher <= RangeToKleptoboss then
-			Driller.Utilities.DebugPrint("Picking DR-CC61 Gorged Gear-Cruncher")
+			DebugPrint("Picking DR-CC61 Gorged Gear-Cruncher")
 			ProjectID = "DR-CC61" -- "Gorged Gear-Cruncher"
 			if RangeToGearCruncher >= MAX_RANGE_FOR_ID then InRange = false end
 		elseif RangeToMechaslime <= RangeToGearCruncher and RangeToMechaslime <= RangeToKleptoboss then
-			Driller.Utilities.DebugPrint("Picking DR-CC73 Caustic Mechaslime")
+			DebugPrint("Picking DR-CC73 Caustic Mechaslime")
 			ProjectID = "DR-CC73" -- "Caustic Mechaslime"
 			if RangeToMechaslime >= MAX_RANGE_FOR_ID then InRange = false end
 		else
-			Driller.Utilities.DebugPrint("Picking DR-CC88 Kleptoboss")
+			DebugPrint("Picking DR-CC88 Kleptoboss")
 			ProjectID = "DR-CC88" -- "Kleptoboss"
 			if RangeToKleptoboss >= MAX_RANGE_FOR_ID then InRange = false end
 		end
@@ -210,21 +278,21 @@ local function OnTooltipSetUnit(self, data)
 		-- could be:
 		-- "DR-JD41", -- "Boilburn"
 		-- "DR-JD99", -- "Gemicide"
-		Driller.Utilities.DebugPrint("In JD block.")
+		DebugPrint("In JD block.")
 
 		-- Find out which is closer
 		local RangeToBoilburn = HBD:GetWorldDistance(MECHAGON_MAPID, PlayerX, PlayerY, Driller.Projects["DR-JD41"].WorldLoc.x, Driller.Projects["DR-JD41"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToBoilburn is " .. Driller.Projects["DR-JD41"].WorldLoc.x .. ", " .. Driller.Projects["DR-JD41"].WorldLoc.y .. ", " .. RangeToBoilburn)
+		DebugPrint("MobX, MobY, RangeToBoilburn is " .. Driller.Projects["DR-JD41"].WorldLoc.x .. ", " .. Driller.Projects["DR-JD41"].WorldLoc.y .. ", " .. RangeToBoilburn)
 
 		local RangeToGemicide = HBD:GetWorldDistance(MECHAGON_MAPID, PlayerX, PlayerY, Driller.Projects["DR-JD99"].WorldLoc.x, Driller.Projects["DR-JD99"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToGemicide is " .. Driller.Projects["DR-JD99"].WorldLoc.x .. ", " .. Driller.Projects["DR-JD99"].WorldLoc.y .. ", " .. RangeToGemicide)
+		DebugPrint("MobX, MobY, RangeToGemicide is " .. Driller.Projects["DR-JD99"].WorldLoc.x .. ", " .. Driller.Projects["DR-JD99"].WorldLoc.y .. ", " .. RangeToGemicide)
 
 		if RangeToBoilburn < RangeToGemicide then
-			Driller.Utilities.DebugPrint("Picking DR-JD41 Boilburn")
+			DebugPrint("Picking DR-JD41 Boilburn")
 			ProjectID = "DR-JD41" -- "Boilburn"
 			if RangeToBoilburn >= MAX_RANGE_FOR_ID then InRange = false end
 		else
-			Driller.Utilities.DebugPrint("Picking DR-JD99 Gemicide")
+			DebugPrint("Picking DR-JD99 Gemicide")
 			ProjectID = "DR-JD99" -- "Gemicide"
 			if RangeToGemicide >= MAX_RANGE_FOR_ID then InRange = false end
 		end
@@ -233,47 +301,47 @@ local function OnTooltipSetUnit(self, data)
 		-- could be:
 		-- "DR-TR28", -- "Ol' Big Tusk"
 		-- "DR-TR35", -- "Earthbreaker Gulroc"
-		Driller.Utilities.DebugPrint("In TR block.")
+		DebugPrint("In TR block.")
 
 		-- Find out which is closer
 		local RangeToBigTusk = HBD:GetWorldDistance(MECHAGON_MAPID, PlayerX, PlayerY, Driller.Projects["DR-TR28"].WorldLoc.x, Driller.Projects["DR-TR28"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToBigTusk is " .. Driller.Projects["DR-TR28"].WorldLoc.x .. ", " .. Driller.Projects["DR-TR28"].WorldLoc.y .. ", " .. RangeToBigTusk)
+		DebugPrint("MobX, MobY, RangeToBigTusk is " .. Driller.Projects["DR-TR28"].WorldLoc.x .. ", " .. Driller.Projects["DR-TR28"].WorldLoc.y .. ", " .. RangeToBigTusk)
 
 		local RangeToGulroc = HBD:GetWorldDistance(MECHAGON_MAPID, PlayerX, PlayerY, Driller.Projects["DR-TR35"].WorldLoc.x, Driller.Projects["DR-TR35"].WorldLoc.y)
-		Driller.Utilities.DebugPrint("MobX, MobY, RangeToGulroc is " .. Driller.Projects["DR-TR35"].WorldLoc.x .. ", " .. Driller.Projects["DR-TR35"].WorldLoc.y .. ", " .. RangeToGulroc)
+		DebugPrint("MobX, MobY, RangeToGulroc is " .. Driller.Projects["DR-TR35"].WorldLoc.x .. ", " .. Driller.Projects["DR-TR35"].WorldLoc.y .. ", " .. RangeToGulroc)
 
 		if RangeToBigTusk < RangeToGulroc then
-			Driller.Utilities.DebugPrint("Picking DR-TR28 Ol' Big Tusk")
+			DebugPrint("Picking DR-TR28 Ol' Big Tusk")
 			ProjectID = "DR-TR28" -- "Ol' Big Tusk"
 			if RangeToBigTusk >= MAX_RANGE_FOR_ID then InRange = false end
 		else
-			Driller.Utilities.DebugPrint("Picking DR-TR35 Earthbreaker Gulroc")
+			DebugPrint("Picking DR-TR35 Earthbreaker Gulroc")
 			ProjectID = "DR-TR35" -- "Earthbreaker Gulroc"
 			if RangeToGulroc >= MAX_RANGE_FOR_ID then InRange = false end
 		end
 
 	elseif 135497 == NPCID then
 		-- real mushroom that spawns Fungarian Furor
-		--Driller.Utilities.DebugPrint("Found real mushroom.")
+		--DebugPrint("Found real mushroom.")
 		Driller:AddTooltipLine(L["FUROR"]:format(Driller.Utilities.CHAT_GREEN .. L["Fungarian Furor"] .. FONT_COLOR_CODE_CLOSE))
 		return
 
 	elseif 151893 == NPCID then
 		-- fake mushroom that spawns random trash
-		--Driller.Utilities.DebugPrint("Found fake mushroom.")
+		--DebugPrint("Found fake mushroom.")
 		Driller:AddTooltipLine(L["NOT_FUROR"]:format(Driller.Utilities.CHAT_RED .. L["Fungarian Furor"] .. FONT_COLOR_CODE_CLOSE))
 		return
 
 	else
 		-- not a tracked ID
-		--Driller.Utilities.DebugPrint("Not a tracked NPC.")
+		--DebugPrint("Not a tracked NPC.")
 		return
 	end
 
 	-- Make sure we got a valid project. If somehow we didn't, bail out.
 	if not ProjectID then return end
 
-	Driller.Utilities.DebugPrint("NPCID is " .. NPCID ..", ProjectID is " .. ProjectID)
+	DebugPrint("NPCID is " .. NPCID ..", ProjectID is " .. ProjectID)
 
 	-- Convert to a usable project object to get the mob name.
 	local Project = Driller.Projects[ProjectID]
@@ -282,7 +350,7 @@ local function OnTooltipSetUnit(self, data)
 		return
 	end
 
-	Driller.Utilities.DebugPrint("match found in MobIDs: " .. Project.Mob)
+	DebugPrint("match found in MobIDs: " .. Project.Mob)
 	if InRange then
 		Driller:AddTooltipLine(L["OPENS_A_PATH"]:format(ProjectID, Driller.Utilities.CHAT_GREEN .. Project.Mob .. FONT_COLOR_CODE_CLOSE))
 	else
@@ -296,9 +364,33 @@ end -- function OnTooltipSetUnit()
 TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, OnTooltipSetUnit)
 
 
+
+
 --#########################################
 --# Events to register and handle
 --#########################################
+
+-- Handle login and initialization stuff.
+function Driller.Events:PLAYER_LOGIN(...)
+
+	-- Initialize saved variables
+	if not DrillSergeant_DB then DrillSergeant_DB = {} end
+	if not DrillSergeant_DB.mode then
+		if TomTom then
+			DrillSergeant_DB.mode = "TomTom"
+		else
+			DrillSergeant_DB.mode = "WoW"
+		end
+	end
+
+	-- Determine waypoint mode
+	if "TomTom" == DrillSergeant_DB.mode and TomTom then
+		Driller.mode = "TomTom"
+	else
+		Driller.mode = "WoW"
+	end
+end -- Driller.Events:PLAYER_LOGIN()
+
 
 -- This triggers when an NPC gives an emote.
 function Driller.Events:CHAT_MSG_MONSTER_EMOTE(...)
@@ -308,29 +400,27 @@ function Driller.Events:CHAT_MSG_MONSTER_EMOTE(...)
 	if not Map or not MECHAGON_SUB_MAP_IDS[Map] then return end
 
 	local message, sender = ...
-	Driller.Utilities.DebugPrint("Got CHAT_MSG_MONSTER_EMOTE")
-	Driller.Utilities.DebugPrint("message is >>" .. message .. "<<")
-	Driller.Utilities.DebugPrint("sender is >>" .. sender .. "<<")
+	DebugPrint("Got CHAT_MSG_MONSTER_EMOTE")
+	DebugPrint("message is >>" .. message .. "<<")
+	DebugPrint("sender is >>" .. sender .. "<<")
 
 	-- Parse the message to see whether it is a drill rig announcement.
-	local DrillID = string.match(message, L["DRILL_RIG_MSG_CAPTURE"])
+	local LocalizedDrillID = string.match(message, L["DRILL_RIG_MSG_CAPTURE"])
 
-	if DrillID then
-		Driller.Utilities.DebugPrint("Identified localized language DrillID " .. DrillID)
+	if LocalizedDrillID then
+		DebugPrint("Identified localized language DrillID " .. LocalizedDrillID)
 
 		-- Convert the Drill ID from its localized version to English
-		DrillID = DrillRigInEnglish[DrillID]
-		Driller.Utilities.DebugPrint("Converted DrillID to English: " .. DrillID)
+		local DrillID = DrillRigInEnglish[LocalizedDrillID]
+		DebugPrint("Converted DrillID to English: " .. DrillID)
 
 		if Driller.Projects[DrillID] then
-			Driller.Utilities.DebugPrint("mob is >>" .. Driller.Projects[DrillID].Mob .. "<<")
-			local Loc = Driller.Projects[DrillID].Loc.x .. ", " .. Driller.Projects[DrillID].Loc.y
-			Driller.Utilities.DebugPrint("Loc is >>" .. Loc .. "<<")
-
-			local LocWithLink = string.format("|cffffff00|Hworldmap:%s:%s:%s|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a %s]|h|r", MECHAGON_MAPID, Driller.Projects[DrillID].Loc.x * 100, Driller.Projects[DrillID].Loc.y * 100, Loc)
-			Driller.Utilities.DebugPrint("LocWithLink is >>" .. LocWithLink .. "<<")
-
 			-- Found a proper drill message. Notify the user.
+			DebugPrint("mob is >>" .. Driller.Projects[DrillID].Mob .. "<<")
+
+			local LocWithLink = CreateWaypointLink(MECHAGON_MAPID, Driller.Projects[DrillID].Loc.x, Driller.Projects[DrillID].Loc.y, LocalizedDrillID)
+
+			-- Print the notification with the link.
 			Driller.Utilities.ChatPrint(L["ABOUT_TO_SPAWN"]:format(
 				Driller.Utilities.CHAT_GREEN .. Driller.Projects[DrillID].Mob .. FONT_COLOR_CODE_CLOSE,
 				LocWithLink
@@ -339,9 +429,10 @@ function Driller.Events:CHAT_MSG_MONSTER_EMOTE(...)
 			Driller.Utilities.ChatPrint(L["UNKNOWN_DRILL_ID"]:format(DrillID))
 		end
 	else
-		Driller.Utilities.DebugPrint("Not a drill message.")
+		DebugPrint("Not a drill message.")
 	end
 end -- Driller.Events:CHAT_MSG_MONSTER_EMOTE()
+
 
 
 --#########################################
@@ -355,14 +446,26 @@ end)
 
 -- Register all events for which handlers have been defined
 for k, v in pairs(Driller.Events) do
-	Driller.Utilities.DebugPrint("Registering event " .. k)
+	DebugPrint("Registering event " .. k)
 	Driller.Frame:RegisterEvent(k)
 end
 
 
 --#########################################
---# Create command line for debug mode toggling
+--# Create command line handler
 --#########################################
+
+local function PrintWaypointStatus()
+	local ModeName
+
+	if "TomTom" == Driller.mode then
+		ModeName = L["TomTom's crazy arrow"]
+	else
+		ModeName = L["WoW's waypoints"]
+	end
+	Driller.Utilities.ChatPrint(L["LINK_FORMAT_CONFIRMATION"]:format(ModeName))
+end -- PrintWaypointStatus()
+
 
 -- Toggle debug mode if asked
 function Driller.CommandLine(arg, ...)
@@ -374,12 +477,45 @@ function Driller.CommandLine(arg, ...)
 		else
 			Driller.Utilities.ChatPrint("Debug mode is now " .. Driller.Utilities.CHAT_RED .. "off" .. FONT_COLOR_CODE_CLOSE .. ".")
 		end
-	else
+
+	elseif "PRINTLINK" == arg:upper() then
+		local LocWithLink = CreateWaypointLink(MECHAGON_MAPID, 68.4, 48.1, "Fake Kleptoboss")
+		DebugPrint("Raw link: " .. LocWithLink:gsub("|", "||"))
+
+		-- Print the notification with the link.
+		Driller.Utilities.ChatPrint(L["ABOUT_TO_SPAWN"]:format(
+			Driller.Utilities.CHAT_GREEN .. "Fake Kleptoboss" .. FONT_COLOR_CODE_CLOSE,
+			LocWithLink
+		))
+
+	-- Enable TomTom support
+	elseif "TOMTOM" == arg:upper() then
+		if TomTom then
+			DrillSergeant_DB.mode = "TomTom"
+			Driller.mode = "TomTom"
+			PrintWaypointStatus()
+		else
+			Driller.Utilities.ChatPrint("TomTom is not loaded, so it cannot be used.")
+			PrintWaypointStatus()
+		end
+
+	-- enable WoW waypoint support
+	elseif "WOW" == arg:upper() then
+		DrillSergeant_DB.mode = "WoW"
+		Driller.mode = "WoW"
+		PrintWaypointStatus()
+
+	-- Unknown argument
+	elseif arg and arg ~= "" then
 		Driller.Utilities.ChatPrint("Unrecognized command: " .. arg)
+
+	-- If no argument, just print status
+	else
+		PrintWaypointStatus()
 	end
 end -- Driller.CommandLine()
 
 
 -- Set the default slash command.
-SLASH_DRILLER1 = "/driller"
-SlashCmdList.DRILLER = function (...) Driller.CommandLine(...) end
+SLASH_DRILL1 = "/drill"
+SlashCmdList.DRILL = function (...) Driller.CommandLine(...) end
